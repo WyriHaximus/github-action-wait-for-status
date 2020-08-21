22067311ded781baf1e0a1604367b3f0c44041c0
Cees-Jan Kiewiet <ceesjank@gmail.com>
2020-08-21 20:38:25 +0200

    Resolve small possible race condition in WaitForStatusCheckResult

--- src/WaitForStatusCheckResult.php
+++ src/WaitForStatusCheckResult.php
@@ -26,8 +26,8 @@ final class WaitForStatusCheckResult
         return new Promise(function (callable $resolve, callable $reject) use ($statusCheck): void {
             $this->loop->addPeriodicTimer($this->interval, function (TimerInterface $timer) use ($statusCheck, $resolve): void {
                 if ($statusCheck->hasResolved()) {
-                    $resolve($statusCheck->isSuccessful());
                     $this->loop->cancelTimer($timer);
+                    $resolve($statusCheck->isSuccessful());
 
                     return;
                 }
